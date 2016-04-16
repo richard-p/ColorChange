2e229820ae9c8d9ecf4cb7db141bc61759270a85
Richard <Richard>
2016-04-15 22:08:31 -0400

    "Added comments for better documentation"

--- ColorChange.ino
+++ ColorChange.ino
@@ -1,13 +1,13 @@
-int PIN = A0;
-unsigned int SIG = 0;
+int PIN = A0;            //analog pin connected to potentiometer
+unsigned int SIG = 0;    //unsigned int to be able to contain 0-65535 (0xffff) (2 bytes)
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(PIN,INPUT);
+  Serial.begin(9600);    //common baud rate for arduino
+  pinMode(PIN,INPUT);    // set analog pin as an input
 }
 
 void loop() {
-   SIG = analogRead(PIN);
-   SIG = map(SIG, 0, 1023, 0, 65535);
-   Serial.println(SIG);
+   SIG = analogRead(PIN);     //read analog pin
+   SIG = map(SIG, 0, 1023, 0, 65535);  // Scale potentiometer values to hex values
+   Serial.println(SIG);    // print scaled signal values to serial
 }
--- ColorChange.py
+++ ColorChange.py
@@ -1,15 +1,24 @@
 import serial, webbrowser
+
+# html code to be written to file
 Code = "<!DOCTYPE html><html><head><title>Potentiometer color changer</title></head><body><h1><font color='%s'>Color</font></h1></body></html>"
+
+#counter to limit number of changes
 count = 0
-#Something has to be connected to port.
+
 while count<10:
+	#Something has to be connected to port.
 	with serial.Serial('COM12', 9600) as data:
-		color = int(data.readline())
-		print color
-		newCode = Code %hex(color)
+		# color comes in as a str, convert to int, convert to hex
+		color = hex(int(data.readline()))
+		newCode = Code %color
 		
 	with open('ColorChange.html','w+') as HTMLfile:
+		#got to beginning of file (go zero from zero)
 		HTMLfile.seek(0,0)
+		#erase contents/write newCode
 		HTMLfile.write(newCode)
+
+	#open new html file in browser
 	webbrowser.open('C:\Users\Richard\Desktop\ColorChange\ColorChange.html',2,True)
-	count+=1
+	count+=1 #increment counter
